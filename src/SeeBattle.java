d07b23d32f9ccd4466b8cd8efe2f61d740dda685
Brendyus <brendyus@gmail.com>
2021-08-13 10:35:26 +0300

    поиграем в морской бой

--- src/SeeBattle.java
+++ src/SeeBattle.java
@@ -1,2 +1,11 @@
 public class SeeBattle {
+    public static void main(String[] argrs) {
+        int[][] battlearray = new int[5][5];
+        for (int i = 0; i < battlearray.length; i++ ){
+            for (int j = 0; j < battlearray.length; j++){
+                System.out.printf(" %d ", battlearray[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
